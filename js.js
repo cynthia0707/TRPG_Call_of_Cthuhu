7bd60d09010c4257ed4ddce163ddee13d1720674
cynthia0707 <s111206007@stu.ntue.edu.tw>
2025-04-20 17:03:58 +0800

    Update js.js

--- js.js
+++ js.js
@@ -1,19 +1,29 @@
+/* eslint-disable no-console */
 
-function roll3d6() {
+// 通用骰子函式
+function rollDice(times, sides, bonus = 0, multiplier = 1) {
   let total = 0;
-  for (let i = 0; i < 3; i++) {
-    total += Math.floor(Math.random() * 6) + 1;
+  for (let i = 0; i < times; i++) {
+    total += Math.floor(Math.random() * sides) + 1;
   }
-  return total;
+  return (total + bonus) * multiplier;
 }
 
-let result3d6 = roll3d6();
-console.log("你骰出 3D6：" + result3d6);
+// 能力清單 + 骰法設定
+const attributes = [
+  { name: "力量 STR",      roll: () => rollDice(3, 6, 0, 5) },
+  { name: "體質 CON",      roll: () => rollDice(3, 6, 0, 5) },
+  { name: "體型 SIZ",      roll: () => rollDice(2, 6, 6, 5) },
+  { name: "敏捷 DEX",      roll: () => rollDice(3, 6, 0, 5) },
+  { name: "外貌 APP",      roll: () => rollDice(3, 6, 0, 5) },
+  { name: "智力 INT",      roll: () => rollDice(2, 6, 6, 5) },
+  { name: "意志 POW",      roll: () => rollDice(3, 6, 0, 5) },
+  { name: "教育 EDU",      roll: () => rollDice(2, 6, 6, 5) },
+  { name: "幸運 LUCK",    roll: () => rollDice(3, 6, 0, 5) }
+];
 
-function roll1d6() {
-  return Math.floor(Math.random() * 6) + 1;
-}
-
-let result1d6 = roll1d6();
-console.log("你骰出了 1D6：" + result1d6);
-
+// 執行所有屬性擲骰並輸出結果
+attributes.forEach(attr => {
+  const result = attr.roll();
+  console.log(`${attr.name}：${result}`);
+});
