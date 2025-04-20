9f35354b1ccae57710c6a378f46955a1c43dc331
cynthia0707 <s111206007@stu.ntue.edu.tw>
2025-04-20 16:34:02 +0800

    Update js.js

--- js.js
+++ js.js
@@ -1,4 +1,4 @@
-//你好
+
 function roll3d6() {
   let total = 0;
   for (let i = 0; i < 3; i++) {
@@ -7,5 +7,13 @@ function roll3d6() {
   return total;
 }
 
-let result = roll3d6();
-console.log("你骰出：" + result);
+let result3d6 = roll3d6();
+console.log("你骰出 3D6：" + result3d6);
+
+function roll1d6() {
+  return Math.floor(Math.random() * 6) + 1;
+}
+
+let result1d6 = roll1d6();
+console.log("你骰出了 1D6：" + result1d6);
+
