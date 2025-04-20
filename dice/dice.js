d1785df9092ef00616ea9bc9755fae7d31d76cdb
cynthia0707 <s111206007@stu.ntue.edu.tw>
2025-04-21 00:24:27 +0800

    Update dice.js

--- dice/dice.js
+++ dice/dice.js
@@ -5,3 +5,12 @@ function rollDice(times, sides, bonus = 0, multiplier = 1) {
     }
     return (total + bonus) * multiplier;
   }
+  fetch("../components/navbar.html")
+  .then(res => {
+    if (!res.ok) throw new Error("載入 navbar 失敗！");
+    return res.text();
+  })
+  .then(data => {
+    document.getElementById("navbar-placeholder").innerHTML = data;
+  })
+  .catch(err => console.error(err));
